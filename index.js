44c27a1a2b4f3a34b1606511c20254f96277d2b6
Nicorabi <[200~121673697+Nicorabi@users.noreply.github.com~>
2023-01-22 21:59:36 +1100

    Added function getComputerChoice

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello Odin!")
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissor'];
+    let choice = choices[Math.floor(Math.random()*choices.length)];
+    console.log('The computer chose: ' + choice);
+}
+getComputerChoice()
