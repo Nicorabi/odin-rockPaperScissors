9523d6221f90c0ad305a249866980b363f81e6f8
Nicorabi <[200~121673697+Nicorabi@users.noreply.github.com~>
2023-01-23 22:40:40 +1100

    Updated js with some temp code

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissor'];
+    let choices = ["Rock", "Paper", "Scissor"];
     let choice = choices[Math.floor(Math.random()*choices.length)];
-    console.log('The computer chose: ' + choice);
+    console.log("The computer chose: " + choice);
 }
 getComputerChoice()
 
 function playRound(playerSelection, computerSelection) {
-    let playerChoices = prompt("Rock, paper or scissors? ")
-    return playerChoices = playerChoices.toUpperCase();
+    let playerChoices = prompt("Rock, Paper or Scissor? ");
 }
 playRound();
+
+let choices = ["Rock", "Paper", "Scissor"];
+let choice = choices[Math.floor(Math.random()*choices.length)];
+let playerChoices = prompt("Rock, Paper or Scissor? ");
+console.log("The computer chose: " + choice);
+
+if (playerChoices === "Rock" && choice === "Rock") {
+    console.log("Draw!");
+} else if (playerChoices === "Rock" && choice === "Scissor") {
+    console.log("Player wins!");
+} else if (playerChoices === "Rock" && choice === "Paper") {
+    console.log("Player loses!");
+} else if (playerChoices === "Paper" && choice === "Paper") {
+    console.log("Draw!");
+} else if (playerChoices === "Paper" && choice === "Rock") {
+    console.log("Player wins!");
+} else if (playerChoices === "Paper" && choice === "Scissor") {
+    console.log("Player loses!");
+} else if (playerChoices === "Scissor" && choice === "Scissor") {
+    console.log("Draw!");
+} else if (playerChoices === "Scissor" && choice === "Rock") {
+    console.log("Player loses!");
+} else if (playerChoices === "Scissor" && choice === "Paper") {
+    console.log("Player wins!");
+} else {
+    console.log("Invalid input by player");
+}
