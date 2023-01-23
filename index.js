865a0be6fa7960f1d8356a208167cea42871aa76
Nicorabi <[200~121673697+Nicorabi@users.noreply.github.com~>
2023-01-23 21:20:30 +1100

    Added playRound function

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function getComputerChoice() {
     console.log('The computer chose: ' + choice);
 }
 getComputerChoice()
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoices = prompt("Rock, paper or scissors? ")
+    return playerChoices = playerChoices.toUpperCase();
+}
+playRound();
